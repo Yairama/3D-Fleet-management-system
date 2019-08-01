957547e924e74215a589290a0f71148bb5888905
julio <jhuarcayar@uni.pe>
2019-07-31 19:21:26 -0500

    yuiutilhandlername added
    yuiutilhandlername added

---
+++ ProjectMizuno/src/com/yui/lib/yuiutil/YuiutilOHandlerName.java
@@ -0,0 +1,42 @@
+package com.yui.lib.yuiutil;
+import java.io.*;
+
+public class YuiutilIOHandlerName {
+
+    public String              name                     = null;
+
+    public String              baseurl                  = null;
+
+    public int                 gettype                  = 0;
+
+    public String              src                      = null;
+
+    public String              srcfile                  = null;
+
+    public String              srcurl                   = null;
+
+    public int                 puttype                  = 0;
+
+    public String              dst                      = null;
+
+    public String              dstfile                  = null;
+
+    public String              dsturl                   = null;
+
+    public InputStream         is                       = null;
+
+    //input asociado
+
+    public OutputStream        os                       = null;
+
+    public  YuiutilIOHandlerName()  {
+    }
+
+    public  YuiutilIOHandlerName(String argstr_name) {
+        name  = argstr_name;
+    }
+
+    public  String toString()  {
+        return "YuiutilIOHandlerName=[" + name + "|" + baseurl + "|" + src + "|" + srcfile + "|" + srcurl + "|" + is + "|" + os + "]";
+    }
+}
---
+++ ProjectMizuno/src/com/yui/yuicad/app/yuicadtest/yuicadtest.java
@@ -0,0 +1,7 @@
+package com.yui.yuicad.app.yuicadtest;
+import java.applet.*;
+import java.awt.*;
+import java.util.*;
+
+import com.yui.lib.yuiutil.*;
+import com.yui.yuicad.lib.yuixxf.*;
